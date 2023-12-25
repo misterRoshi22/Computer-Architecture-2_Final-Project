27d077c91550ced95c62e12e2909dd9c90ebf45e
Mahmoud Abu-Qtiesh <109997866+misterRoshi22@users.noreply.github.com>
2023-12-25 10:10:00 +0300

    Create test1.asm

---
+++ assembler/test1.asm
@@ -0,0 +1,17 @@
+addi x2 zero 0
+addi x3 zero 0
+addi x1 x1 52
+addi x2 x2 21
+addi x3 x3 33
+addi x4 x4 2
+ori x5 x1 1
+andi x6 x2 63
+add x7 x2 ,x2
+or x8 x2 x3
+xor x9 x1 x2
+slt x10 x2 x3
+sll x11 x1 x4
+srl x12 x1 x4
+sub x13 x2 x1
+sub x14 x2 x1
+lui x15 349525

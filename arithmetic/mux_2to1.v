3900d7d34cd591c17286e2f6dc63e9d1e75e186a
Mahmoud Abu-Qtiesh <109997866+misterRoshi22@users.noreply.github.com>
2023-12-31 01:22:09 +0300

    Update mux_2to1.v

--- arithmetic/mux_2to1.v
+++ arithmetic/mux_2to1.v
@@ -1,10 +1,10 @@
 module mux_2to1 (A, B, S, M);
 
-	 parameter AB_width = 32;
-    input  [AB_width-1:0] A;
-    input  [AB_width-1:0] B;
-    input  S;
-    output [AB_width-1:0] M;
+	parameter AB_width = 32;
+	input  [AB_width-1:0] A;
+    	input  [AB_width-1:0] B;
+    	input  S;
+    	output [AB_width-1:0] M;
 	 
 	 assign M =  S ? B : A;
 

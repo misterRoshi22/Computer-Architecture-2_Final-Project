5ab8d73f04b962823293043200b61b20a895153b
Mahmoud Abu-Qtiesh <109997866+misterRoshi22@users.noreply.github.com>
2023-12-31 01:21:37 +0300

    Update immediate_gen.v

--- arithmetic/immediate_gen.v
+++ arithmetic/immediate_gen.v
@@ -13,12 +13,12 @@ module immediate_gen(instruction, immediate);
    always @(*)  begin
 	
    case(OpCode)
-        7'h1b: IMM_OUT <= { {21{instruction[31]}}, instruction[30:25], instruction[24:20]};   													// ADDI     -> I-Type
-        7'h13: IMM_OUT <= { {21{instruction[31]}}, instruction[30:25], instruction[24:20]};														// ORI/ANDI -> I-Type
-		  7'h03: IMM_OUT <= { {21{instruction[31]}}, instruction[30:25], instruction[24:20]};														// LW/LB		-> I-Type
+        7'h1b: IMM_OUT <= { {21{instruction[31]}}, instruction[30:25], instruction[24:20]};   								// ADDI     	-> I-Type
+        7'h13: IMM_OUT <= { {21{instruction[31]}}, instruction[30:25], instruction[24:20]};								// ORI/ANDI 	-> I-Type
+	7'h03: IMM_OUT <= { {21{instruction[31]}}, instruction[30:25], instruction[24:20]};								// LW/LB	-> I-Type
 		  
-		  7'h23: IMM_OUT <= { {21{instruction[31]}}, instruction[30:25], instruction[11:7]};     													// SW/SB    -> S-Type
-        7'h38: IMM_OUT <= { instruction[31], instruction[30:12], {12{1'b0}} };             														// LUI	   -> U-Type
+	 7'h23: IMM_OUT <= { {21{instruction[31]}}, instruction[30:25], instruction[11:7]};     							// SW/SB    	-> S-Type
+        7'h38: IMM_OUT <= { instruction[31], instruction[30:12], {12{1'b0}} };             								// LUI	   	-> U-Type
            												
         7'h6F: IMM_OUT <= { {12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], {1{1'b0}}};  				// JAL 		-> UJ-Type
         7'h63: IMM_OUT <= { {20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], {1{1'b0}}};  					// BEQ/BNE	-> SB-Type

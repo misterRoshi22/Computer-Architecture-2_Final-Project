1c87d34a28babcac91efa585241fe18734418087
Mahmoud Abu-Qtiesh <109997866+misterRoshi22@users.noreply.github.com>
2023-12-31 02:12:20 +0300

    Update hazard_detection_unit.v

--- control/hazard_detection_unit.v
+++ control/hazard_detection_unit.v
@@ -15,16 +15,8 @@ OPCODE, control_MUX_select, IF_ID_REG_Write, PC_Write);
 			PC_Write <= 1;
 			IF_ID_REG_Write <= 1;
 		
-		//Hazard Detected if EXE register == ID Register and instruction on Load
-		//In this case we have to stall the IF stage so the appropriate control signals are sent
-		if(OPCODE == 7'h1b || OPCODE == 7'h13 || OPCODE == 7'h03 || OPCODE == 7'h6f || OPCODE == 7'h67 || OPCODE == 7'h38 ) begin
-			control_MUX_select <= 0;
-			PC_Write <= 1;
-			IF_ID_REG_Write <= 1;
-		
-		end
-		
-		else if(ID_EXE_MemRead && ((ID_EXE_rd == IF_ID_rs1)||(ID_EXE_rd == IF_ID_rs2))) begin
+
+		if(ID_EXE_MemRead && ((ID_EXE_rd == IF_ID_rs1)||(ID_EXE_rd == IF_ID_rs2))) begin
 			control_MUX_select <= 1;
 			PC_Write <= 0;
 			IF_ID_REG_Write <= 0;

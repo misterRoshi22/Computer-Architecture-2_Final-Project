d9892b6976f277a85922f9f3af15a900b6680d95
Mahmoud Abu-Qtiesh <109997866+misterRoshi22@users.noreply.github.com>
2023-12-31 02:09:29 +0300

    Update shift_left_by_1.v

--- arithmetic/shift_left_by_1.v
+++ arithmetic/shift_left_by_1.v
@@ -1,14 +1,14 @@
 module shift_left_by_1(input_value, shifted_output);
 
-	input [31:0] input_value;
-    	output [14:0] shifted_output;
+	 input [31:0] input_value;
+   	 output [14:0] shifted_output;
 	 
-	reg [31:0] SHIFTED_OUTPUT;
+	 reg [31:0] SHIFTED_OUTPUT;
 	 
-	assign shifted_output = SHIFTED_OUTPUT[14:0]; // output is truncated to match size of PC
+	 assign shifted_output = SHIFTED_OUTPUT[14:0]; // output is truncated to match size of PC
 
-    	always @(*) begin
+   	 always @(*) begin
         	SHIFTED_OUTPUT = input_value << 1;	// input is multiplied by 2 to become a multiple of 4 for safer instruction memory addressing
-    	end
+   	 end
 
 endmodule // shift_left_by_1

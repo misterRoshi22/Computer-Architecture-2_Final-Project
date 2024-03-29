9b1c407944b8cea16974c7fb3ece75de425d4ed6
Mahmoud Abu-Qtiesh <109997866+misterRoshi22@users.noreply.github.com>
2024-03-29 04:31:56 +0300

    Update immediate_gen.v

--- arithmetic/immediate_gen.v
+++ arithmetic/immediate_gen.v
@@ -16,8 +16,9 @@ module immediate_gen(instruction, immediate);
         7'h1b: IMM_OUT <= { {21{instruction[31]}}, instruction[30:25], instruction[24:20]};   								// ADDI     	-> I-Type
         7'h13: IMM_OUT <= { {21{instruction[31]}}, instruction[30:25], instruction[24:20]};								// ORI/ANDI 	-> I-Type
 	7'h03: IMM_OUT <= { {21{instruction[31]}}, instruction[30:25], instruction[24:20]};								// LW/LB	-> I-Type
-		  
-	 7'h23: IMM_OUT <= { {21{instruction[31]}}, instruction[30:25], instruction[11:7]};     							// SW/SB    	-> S-Type
+	7'h67: IMM_OUT <= { {21{instruction[31]}}, instruction[30:25], instruction[24:20]};								// JALR		-> I-Type
+	   
+	7'h23: IMM_OUT <= { {21{instruction[31]}}, instruction[30:25], instruction[11:7]};     								// SW/SB    	-> S-Type
         7'h38: IMM_OUT <= { instruction[31], instruction[30:12], {12{1'b0}} };             								// LUI	   	-> U-Type
            												
         7'h6F: IMM_OUT <= { {12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], {1{1'b0}}};  				// JAL 		-> UJ-Type
